f2e605f616123b5e04e19187832ea10c278babdd
Pierre Poulain <pierre.poulain@cupnet.net>
2025-11-26 17:49:35 +0100

    Use dataframe for data aggregation

--- src/count_entities_per_class.py
+++ src/count_entities_per_class.py
@@ -18,11 +18,11 @@ __date__ = "2025"
 __version__ = "1.0.0"
 
 
-import csv
 import json
 from pathlib import Path
 from typing import Dict, List
 
+import pandas as pd
 from loguru import logger
 
 ANNOTATION_DIR = Path("annotations/v2")
@@ -69,91 +69,114 @@ def load_json(json_file_path: Path) -> Dict:
         return {}
 
 
-def count_entities_per_class(data: Dict) -> Dict[str, int]:
+def count_entities_per_class(data: Dict, classes: List[str]) -> Dict:
     """
     Count the number of entities per class in a JSON annotation.
 
     Parameters:
     -----------
         data (Dict): The JSON data loaded from an annotation file.
+        classes (List[str]): List of entity classes to count.
 
     Returns:
     --------
-        Dict[str, int]: A dictionary containing the count of entities per class.
+        Dict: Updated dictionary with the count of entities per class.
     """
     # Create empty dictionnary.
-    entity_counts = {cls: 0 for cls in CLASSES}
-
+    record = {cls: 0 for cls in classes}
+    # Count entities per class.
     for entity in data["entities"]:
-        entity_counts[entity["label"]] += 1
+        record[entity["label"]] += 1
+    return record
 
-    return entity_counts
 
-
-def write_tsv(
-    count_entities: List[Dict[str, int]],
-    filenames: List[str],
-    len_files: List[int],
-    output_file: Path,
-) -> None:
+def aggregate(counts_list: List[Dict], classes: List[str]) -> pd.DataFrame:
     """
-    Write the aggregated entity counts into a TSV file.
+    Aggregate a list of entity count dictionaries into a DataFrame.
 
     Parameters:
     -----------
-    count_entities: List[Dict[str, int]]
+    counts_list: List[Dict]
         A list of dictionaries with entity counts.
-    filenames: List[str]
-        A list of corresponding filenames.
-    len_files: List[int]
-        A list of annotated text lenghts.
-    output_file: str
-        Path to the TSV output file.
+    classes: List[str]
+        List of entity classes.
+
+    Returns:
+    --------
+    pd.DataFrame
+        A DataFrame containing the aggregated counts.
     """
-    header = ["filename", "length"] + [f"NB_{cls}" for cls in CLASSES]
+    df = pd.DataFrame(counts_list)
+    columns = {}
+    for cls in classes:
+        columns[cls] = f"NB_{cls}"
+    df = df.rename(columns=columns)
+    df = df[["filename", "length"] + list(columns.values())]
+    return df
 
+
+def display_stats(df: pd.DataFrame, classes: List[str]) -> None:
+    """
+    Display statistics of entity counts per class.
+
+    Parameters:
+    -----------
+    counts_df: pd.DataFrame
+        A DataFrame containing the entity counts.
+    classes: List[str]
+        List of entity classes.
+    """
+    total_entities = df[[f"NB_{cls}" for cls in classes]].sum()
+    logger.info(f"Total number of entities: {total_entities.sum()}")
+    for cls in classes:
+        class_total = df[f"NB_{cls}"].sum()
+        logger.info(f"Number of entities for class '{cls}': {class_total}")
+
+
+def export_to_tsv(
+    df: pd.DataFrame,
+    output_dir: Path,
+) -> None:
+    """
+    Export the entity counts into a TSV file.
+
+    Parameters:
+    -----------
+    df: pd.DataFrame
+        A DataFrame containing the aggregated entity counts.
+    output_dir: Path
+        Directory where the TSV file will be saved.
+        A list of annotated text lenghts.
+    """
+    # Ensure output directory exists.
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True, exist_ok=True)
+    tsv_file_path = output_dir / Path("all_annotations_entities_count.tsv")
     try:
-        with open(output_file, "w", newline="", encoding="utf-8") as tsvfile:
-            writer = csv.DictWriter(tsvfile, fieldnames=header, delimiter="\t")
-            writer.writeheader()
-
-            for filename, counts, length in zip(filenames, count_entities, len_files):
-                row = {"filename": filename, "length": length}
-                for cls in CLASSES:
-                    row[f"NB_{cls}"] = counts.get(cls, 0)
-                writer.writerow(row)
-
-    except Exception as e:
+        df.to_csv(tsv_file_path, sep="\t", index=False)
+    except IOError as e:
         logger.error(f"Failed to write TSV file: {e}")
+    else:
+        logger.success(f"Count results saved in {tsv_file_path}")
 
 
 if __name__ == "__main__":
     logger.info("Searching for JSON files...")
     json_files = list_json_files(ANNOTATION_DIR)
-    count_entities = []
-    filenames = []
-    len_files = []
-    count_all_entities = 0
-    count_all_entities_per_class = {cls: 0 for cls in CLASSES}
+    all_counts = []
     logger.info("Counting entities...")
+    columns = ["filename", "length"] + [f"NB_{cls}" for cls in CLASSES]
+    entities_df = pd.DataFrame(columns=columns)
     for filepath in json_files:
-        data = load_json(filepath)
-        counts = count_entities_per_class(data)
-        count_all_entities += sum(counts.values())
-        for cls, value in counts.items():
-            count_all_entities_per_class[cls] += value
+        json_data = load_json(filepath)
+        counts = count_entities_per_class(json_data, CLASSES)
+        counts["filename"] = filepath.name
+        counts["length"] = len(json_data["raw_text"])
+        all_counts.append(counts)
 
-        count_entities.append(counts)
-        filenames.append(filepath.name)
-        len_files.append(len(data["raw_text"]))
-
-    logger.debug(f"Total number of entities: {count_all_entities}")
-    for cls, value in count_all_entities_per_class.items():
-        logger.info(f"Number of entities for class '{cls}': {value}")
-
-    if not RESULTS_DIR.exists():
-        RESULTS_DIR.mkdir(parents=True, exist_ok=True)
-    tsv_file_path = RESULTS_DIR / Path("all_annotations_entities_count.tsv")
-    write_tsv(count_entities, filenames, len_files, tsv_file_path)
-
-    logger.success(f"Count results saved in {tsv_file_path}")
+    # Aggregate results.
+    counts_df = aggregate(all_counts, CLASSES)
+    # Display statistics.
+    display_stats(counts_df, CLASSES)
+    # Export to TSV.
+    export_to_tsv(counts_df, Path("/"))
